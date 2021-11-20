68c8636bd5624ffe826f58f937f069a18c837948
Ola <olapila@o2.pl>
2021-11-20 10:44:57 +0100

    for each

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         int [] tablica = {6,5,4,3,2,1};
-        for(int i = 0;i <tablica.length ; i++){
-            if (tablica[i] < 3){
-                System.out.println("True");
-            } else if (tablica[i]<5) {
-                System.out.println("False");
+//        for(int i = 0;i <tablica.length ; i++){
+//            if (tablica[i] < 3){
+//                System.out.println("True");
+//            } else if (tablica[i]<5) {
+//                System.out.println("False");
+        for (int liczba:tablica){
+            if (liczba>3 && liczba>5)
+            System.out.println(liczba);
 
             }
         };
+//    String [] cars = {"Volvo" , "Audi" , "Chevrolet" , "BMW"};
+//    for(String car:cars){
+//        System.out.println(car);
+//    };
+
     }
-}
+

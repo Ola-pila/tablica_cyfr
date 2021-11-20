fb4f9ac710ec4261ca760ab6dffa7bfed362f6b6
Ola <olapila@o2.pl>
2021-11-20 11:01:27 +0100

    do while

--- src/Main.java
+++ src/Main.java
@@ -27,13 +27,18 @@ public class Main {
 //                break;
 //            default:
 //                System.out.println("Weekend");
-      int i = 0;
-      while (i<5){
-          System.out.println(i);
-          i++;
-      }
+//      int i = 0;
+//      while (i<5){
+//          System.out.println(i);
+//          i++;
+//      }
 
-        }
+        int j = 0;
+        do{
+            System.out.println(j);
+            j++;}
+        while (j<5);
+        };
 
     }
 

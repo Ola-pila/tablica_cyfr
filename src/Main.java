154e4020b234bd629800090966f0f2e4d80c2cef
Ola <olapila@o2.pl>
2021-11-20 10:54:39 +0100

    switch case

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        int [] tablica = {6,5,4,3,2,1};
+//        int [] tablica = {6,5,4,3,2,1};
 //        for(int i = 0;i <tablica.length ; i++){
 //            if (tablica[i] < 3){
 //                System.out.println("True");
 //            } else if (tablica[i]<5) {
 //                System.out.println("False");
-        for (int liczba:tablica){
-            if (liczba>3 && liczba>5)
-            System.out.println(liczba);
-
-            }
-        };
+//        for (int liczba:tablica){
+//            if (liczba>3 && liczba>5)
+//            System.out.println(liczba);
+//
+//            }
+//        };
 //    String [] cars = {"Volvo" , "Audi" , "Chevrolet" , "BMW"};
 //    for(String car:cars){
 //        System.out.println(car);
 //    };
 
+        int day = 3;
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Weekend");
+        }
+    }
     }
 
